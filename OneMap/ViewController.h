a387209e9a6decdecdb99a628daebcb0291caa4d
Sirius Dely <siriusdely@icloud.com>
2015-04-16 13:47:44 +0700

    Initial Commit

---
+++ OneMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OneMap
+//
+//  Created by SiriusDely on 4/16/15.
+//  Copyright (c) 2015 Sirius Dely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OneMap/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OneMap
+//
+//  Created by SiriusDely on 4/16/15.
+//  Copyright (c) 2015 Sirius Dely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
